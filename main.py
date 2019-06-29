b639a440732543185757ec4be8901c02ecd0a4a5
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2019-06-30 00:27:09 +0545

    Encapsulated into main function for windows support

--- main.py
+++ main.py
@@ -2,58 +2,46 @@ import wfdb
 from helpers import *
 from multiprocessing import Pool, cpu_count
 
-
-
-outFile = input("Enter name of output file:")
-
 recIds = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 118, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 207, 208, 209, 210, 212, 213, 214, 215, 217, 219, 220, 221, 222, 223, 228, 230, 231, 232, 233, 234]
 
 symbolsIgnore = '+~"'
 
 rsas = {}
 
-queries = []
-for recId in recIds:
-    a  = wfdb.rdann("data/"+str(recId),"atr")
-    for sym,samp in zip(a.symbol,a.sample):
-        query = (recId,samp,sym)
-        queries.append(query)
+def main():
+    outFile = input("Enter name of output file:")
+    queries = []
+    for recId in recIds:
+        a  = wfdb.rdann("data/"+str(recId),"atr")
+        for sym,samp in zip(a.symbol,a.sample):
+            query = (recId,samp,sym)
+            queries.append(query)
 
-pool = Pool( cpu_count() )
-signalss = pool.starmap( getVoltages, queries )
-pool.close()
-pool.join()
+    pool = Pool( cpu_count() )
+    signalss = pool.starmap( getVoltages, queries)
+    pool.close()
+    pool.join()
 
-signals = []
-for signals_ in signalss:
-    if signals_ is not None:
-        signals += signals_
+    signals = []
+    for signals_ in signalss:
+        if signals_ is not None:
+            signals += signals_
 
-print("\nConverting list to string")
-pool = Pool( cpu_count() )
-strings = pool.map( signalListToString , signals )
-pool.close()
-pool.join()
+    print("\nConverting list to string")
+    pool = Pool( cpu_count() )
+    strings = pool.map( signalListToString , signals )
+    pool.close()
+    pool.join()
 
-print("Merging the strings")
-stringToWrite=""
-for item in strings:
-    stringToWrite += item+"\n"
-    
-f = open(outFile,"w+")
-f.write(stringToWrite)
-f.close()
-
-
-
-
+    print("Merging the strings")
+    stringToWrite=""
+    for item in strings:
+        stringToWrite += item+"\n"
+        
+    f = open(outFile,"w+")
+    f.write(stringToWrite)
+    f.close()
 
 
-
-
-
-    
-
-
-
-
+if __name__ == "__main__":
+    main()
